34299ac8c78137d63361cada503cb77db7c2288e
Lisa <kareevaliza@gmail.com>
2026-05-21 09:26:21 +0100

    Add project files.

---
+++ DataManagement/Program.cs
@@ -0,0 +1,49 @@
+﻿using June2024Prep;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PatientData db = new PatientData();
+            try
+            {
+                using (db)
+                {
+                    Patient p1 = new Patient { FirstName = "John", Surname = "Doe", DOB = new DateTime(1990, 1, 1), ContactNumber = "1234567890" };
+                    Patient p2 = new Patient { FirstName = "Jane", Surname = "Smith", DOB = new DateTime(1985, 5, 15), ContactNumber = "0987654321" };
+                    Patient p3 = new Patient { FirstName = "Alice", Surname = "Johnson", DOB = new DateTime(1975, 10, 30), ContactNumber = "5555555555" };
+
+                    Appointment a1 = new Appointment { AppointmentTime = new DateTime(2024, 7, 1, 9, 0, 0), AppointmentNotes = "General Checkup", Patient = p1 };
+                    Appointment a2 = new Appointment { AppointmentTime = new DateTime(2024, 7, 2, 10, 0, 0), AppointmentNotes = "Dental Cleaning", Patient = p2 };
+                    Appointment a3 = new Appointment { AppointmentTime = new DateTime(2024, 7, 3, 11, 0, 0), AppointmentNotes = "Eye Exam", Patient = p3 };
+                    Appointment a4 = new Appointment { AppointmentTime = new DateTime(2024, 7, 4, 14, 0, 0), AppointmentNotes = "Physical Therapy", Patient = p1 };
+
+                    Console.WriteLine("Adding patients and appointments to the database...");
+
+                    db.Patients.Add(p1);
+                    db.Patients.Add(p2);
+                    db.Patients.Add(p3);
+                    db.Appointments.Add(a1);
+                    db.Appointments.Add(a2);
+                    db.Appointments.Add(a3);
+                    db.Appointments.Add(a4);
+
+                    Console.WriteLine("Saving changes to the database...");
+
+                    db.SaveChanges();   
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ June2024Prep/Patient.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace June2024Prep
+{
+    public class Patient
+    {
+        public int PatientID { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public DateTime DOB { get; set; }
+        public string ContactNumber { get; set; }
+
+        // Navigation property to represent the one-to-many relationship with Appointments
+        public virtual List<Appointment> Appointments { get; set; }
+
+    }
+
+    public class Appointment
+    {
+        public int AppointmentID { get; set; }
+        public DateTime AppointmentTime { get; set; }
+        public string AppointmentNotes { get; set; }
+
+        // Foreign key to link to the Patient
+        public int PatientID { get; set; }
+        public virtual Patient Patient { get; set; }
+    }
+
+    public class PatientData : DbContext
+    {
+        //the name of the database
+        public PatientData() : base("OODExam_YelyzavetaKareieva") { }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+    }
+}
